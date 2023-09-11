6411d290acdccc65e8d64b2a2e3c8f60542bcc77
temid22 <104782314+temid22@users.noreply.github.com>
2023-09-11 23:56:37 +0100

    menu page start

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,5 +1,19 @@
+import { menu } from '@/data';
+import Link from 'next/link';
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <section className='p-4 lg:px-20 xl:px-40 h[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
+      {menu.map((category) => (
+        <Link
+          href={category.slug}
+          key={category.id}
+          className=''
+          style={{ backgroundImage: `url(${category.img})` }}
+        ></Link>
+      ))}
+    </section>
+  );
 };
 
 export default Menu;

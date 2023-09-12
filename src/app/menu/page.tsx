7f60c3faf5ffc891196b13d5192bb068f66a8723
temid22 <104782314+temid22@users.noreply.github.com>
2023-09-12 01:00:55 +0100

    menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,11 +6,23 @@ const Menu = () => {
     <section className='p-4 lg:px-20 xl:px-40 h[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center'>
       {menu.map((category) => (
         <Link
-          href={category.slug}
+          href={`/menu/${category.slug}`}
           key={category.id}
-          className=''
+          className='w-full h-1/3 bg-cover p-8 md:h-1/2'
           style={{ backgroundImage: `url(${category.img})` }}
-        ></Link>
+        >
+          <article className={`text-${category.color} w-1/2 `}>
+            <h1 className='uppercase font-bold text-3xl'>{category.title}</h1>
+            <p className='text-sm my-9'>{category.desc}</p>
+            <button
+              className={`hidden 2xl:block bg-${category.color} text-${
+                category.color === 'black' ? 'white' : 'orange-500'
+              } py-2 px-4 rounded-md`}
+            >
+              Explore
+            </button>
+          </article>
+        </Link>
       ))}
     </section>
   );
